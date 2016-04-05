b2d22a0a9da833b0c2c3c7cddc95e62cf097f255
Andreas Demetriou <androsland@gmail.com>
2016-04-05 20:26:45 +0300

    correctly handle windows, unix and openshift environments for deploying the jekyll cms

--- admin/functionality/deploy.php
+++ admin/functionality/deploy.php
@@ -1,13 +1,22 @@
 <?php
+    require_once('../config.php');
     session_start();
     set_time_limit (3600);
 
-    define('JEKYLL_ROOT', $_SERVER['DOCUMENT_ROOT']."/admin/jekyll-cms/");
-    define('ADMIN_ROOT', $_SERVER['DOCUMENT_ROOT']."/admin/");
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+      $bash_install_dir = getenv('BASH_INSTALL_DIR');
+      $deploy_command = $bash_install_dir . ' script.sh';
+      echo $deploy_command;
+      shell_exec($deploy_command);
+    } else if (null !== OPENSHIFT_MYSQL_DB_HOST) {
+      define('JEKYLL_ROOT', $_SERVER['DOCUMENT_ROOT']."/admin/jekyll-cms/");
+      define('ADMIN_ROOT', $_SERVER['DOCUMENT_ROOT']."/admin/");
 
-    $deploy_command = 'export LD_LIBRARY_PATH=/opt/rh/mysql55/root/usr/lib64:/opt/rh/ror40/root/usr/lib64:/opt/rh/ruby200/root/usr/lib64 && export PATH=~/.gem/bin:/opt/rh/ruby200/root/usr/bin:$PATH && gem install bundle && cd '.JEKYLL_ROOT.' && bundle install && bundle exec jekyll build && cp _site/. ../.. -R && rm -rf _site';
-
-    exec($deploy_command);
-
+      $deploy_command = 'export LD_LIBRARY_PATH=/opt/rh/mysql55/root/usr/lib64:/opt/rh/ror40/root/usr/lib64:/opt/rh/ruby200/root/usr/lib64 && export PATH=~/.gem/bin:/opt/rh/ruby200/root/usr/bin:$PATH && gem install bundle && cd '.JEKYLL_ROOT.' && bundle install && bundle exec jekyll build && cp _site/. ../.. -R && rm -rf _site';
+      exec($deploy_command);
+    } else {
+      $deploy_command = './script.sh';
+      exec($deploy_command);
+    }
     $_SESSION['dirty']=false;
 ?>
